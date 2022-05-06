dddda7bf60417057bf2b2e80237bc80cc3bb5710
Piotr-Trzcinski <cinekenic@go2.pl>
2022-05-06 16:31:47 +0200

    slider

---
+++ src/app/services/movies.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MoviesService {
+  constructor(private http: HttpClient) {}
+
+  getMovies() {
+    return this.http.get('https://api.themoviedb.org/3/movie/upcoming?api_key=a6080a69c0f6a762a46bd110c5ca3373');
+  }
+}
